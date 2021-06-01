72c62db03c6e7574b0de0deb78cedba43a14e029
Haein-Han <godls1131@gmail.com>
2021-06-01 22:05:37 +0900

    Create cho_test.ino

---
+++ cho_test.ino
@@ -0,0 +1,27 @@
+int distance;
+int trigger =5;
+int echo=4;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigger,OUTPUT);
+  pinMode(echo,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigger,LOW);
+  digitalWrite(echo,LOW);
+  delay(2);
+
+  digitalWrite(trigger,HIGH);
+  delay(10);
+  digitalWrite(echo,LOW);
+
+  distance=pulseIn(echo,HIGH)/58;
+  Serial.println("Distance="+String(distance));
+  delay(1000);
+
+}
